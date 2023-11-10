0fd50072e7c161771171b95d2bbb5f75b720e0d6
seedit1 <woom1441@naver.com>
2023-11-10 17:13:57 +0900

    sm_feature1

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -1,26 +1,45 @@
 package com.cos.blog.test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+//사용자가 요청 ->  응답(HTML 파일)
+//@Controller
+//사용자가 요청 -> 응답(Data)
 @RestController
 public class HttpControllerTest {
+    private static final String TAG = "HttpControllerTest :" ;
+    @GetMapping("/http/lombok")
+    public String lombokTest(){
+        //Member m = new Member(1, "park", "1234", "park@naver.com");
+        //buider패턴 사용 (순서 상관없이 넣을 수 있다.)
+        Member m = Member.builder().username("park").password("1234").email("park@naver.com").build();
+        //id를 넣지 않았기 때문에 id는 0이 출력된다.
+        System.out.println(TAG + "getter :" + m.getUsername());
+        m.setUsername("seed");
+        System.out.println(TAG + "setter :" + m.getUsername());
+        return "lombok test 완료";
+    }
 
+
+    //인터넷 브라우저 요청은 무조건   get 요청밖에 할 수 없다.
+    //http://localhost:8080/http/get(select)
     @GetMapping("/http/get")
-    public String getTest(Member m){
-        return "get 요청 :" + m.getId() + " , " +  m.getUsername() + " , " + m.getPassword() + " , " + m.getEmail();
-    }
+    public String getTest(Member m){ //id=1&username=seed&password=1234&email=seed@naver.com  스프링이 Member 자동으로 넣어준다.
 
+
+        return "get 요청 :" + m.getId() + " , " + m.getUsername() +  ", " + m.getPassword() + " , " + m.getEmail();
+    }
     @PostMapping("/http/post")
-    public String postTest(@RequestBody Member m){
-        return "post 요청 :" + m.getId() + " , " + m.getUsername() + " , " + m.getPassword() + " , " + m.getEmail();
+    public String postTest(@RequestBody Member m){ //MessageConverter 가 json data로 변환해준다.
+        return "post요청 :" + m.getId() + " , " + m.getUsername() +  ", " + m.getPassword() + " , " + m.getEmail();
     }
-
     @PutMapping("/http/put")
     public String putTest(@RequestBody Member m){
-        return "put 요청 :" + m.getId() + " , " + m.getUsername() + " , " + m.getPassword() + " , " + m.getEmail();
+        return "put 요청 :" + m.getId() + " , " + m.getUsername() +  ", " + m.getPassword() + " , " + m.getEmail();
     }
     @DeleteMapping("/http/delete")
-    public String deleteTest(){
-        return "delete 요청 :";
+    public String deleteTest(@RequestBody Member m){
+        return "delete 요청"+ m.getId() + " , " + m.getUsername() +  ", " + m.getPassword() + " , " + m.getEmail();
     }
 }
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,28 +1,25 @@
 package com.cos.blog.test;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@NoArgsConstructor
+
+//@RequiredArgsConstructor //final이 븥어 있는 변수들에 대해서 생성자를 만들어준다.
+//@AllArgsConstructor //전체 생성자
+@NoArgsConstructor //빈 생성자
+@Data  // @Getter + @Setter
 public class Member {
+    //final은 데이터베이스로 부터 가져온 데이터를 변경되지 않게 하기위해서 붙여준다.
     private int id;
     private String username;
     private String password;
-    private String email;
+    private  String email;
 
-    @Builder
-    public Member(int id, String username, String password, String email){
+   @Builder
+    public Member(int id, String username, String password, String email) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
-    }
+   }
 
-    public Member(String username,String password, String email){
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
 }
